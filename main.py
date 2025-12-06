44908ed100033f2f1a10b4d3f45aa5e34bd3e4a7
Alexander Petersson <alexp.19940228@gmail.com>
2025-12-06 18:02:29 +0100

    New function to count the number of times a character apears

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from stats import number_of_words
-
+from stats import count_char
 
 def main():
     the_book = "./books/frankenstein.txt"
     text = get_book_text(the_book)
     num_words = number_of_words(text)
     print(f"Found {num_words} total words")
+    char_count = count_char(text)
+    print(char_count)
+
 
 def get_book_text(book):
     with open(book) as f:
--- stats.py
+++ stats.py
@@ -1,3 +1,12 @@
 def number_of_words(text):
     text_list = text.split()
     return len(text_list)
+
+def count_char(text):
+    dict_char = {}
+    for char in text.lower():
+        if char not in dict_char:
+            dict_char[char] = 1
+        else:
+            dict_char[char] += 1
+    return dict_char
